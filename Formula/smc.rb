44dbb0373a28fb7326cf3e50dee3a01be5645243
Tomasz Sądej <tomasz@tbx.pl>
2024-04-18 18:56:59 +0200

    refactor: new SMC release

--- Formula/smc.rb
+++ Formula/smc.rb
@@ -1,25 +1,17 @@
 class Smc < Formula
   desc "SMC CLI"
   homepage "https://github.com/tawerna/smc.cli"
-  version "1.1.0"
+  version "1.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tawerna/smc.cli/releases/download/v1.1.0/smc-v1.1.0-darwin-arm64.tar.gz"
-      sha256 "6e2581fcc97c801588105be46ef8990710e96e629f2409e16ec988146818daff"
+      url "https://github.com/tawerna/smc.cli/releases/download/v1.2.0/smc-v1.2.0-darwin-arm64.tar.gz"
+      sha256 "2cc231b84837f18a9074166807f28487810da020cc12b6fbba3a4620148a1c88"
 
       def install
         bin.install "smc"
       end
     end
-#    if Hardware::CPU.intel?
-#      url "https://github.com/tawerna/smc.cli/releases/download/v1.0.1/smc-v1.0.1-darwin-x64.tar.gz"
-#      sha256 ""
-#
-#      def install
-#        bin.install "smc"
-#      end
-#    end
   end
 
   test do
