00c8138beecfab1f0608f36852494956b708a59b
Tomasz Sądej <tomasz@tbx.pl>
2024-04-18 19:00:22 +0200

    fix: wrong checksums

--- Formula/smc.rb
+++ Formula/smc.rb
@@ -6,7 +6,7 @@ class Smc < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tawerna/smc.cli/releases/download/v1.2.0/smc-v1.2.0-darwin-arm64.tar.gz"
-      sha256 "2cc231b84837f18a9074166807f28487810da020cc12b6fbba3a4620148a1c88"
+      sha256 "6442d8737aca64aadb0c85d217494bbb01721754130815237c3b88bb1005a825"
 
       def install
         bin.install "smc"
