572d755c0e7abbd367797a96bb26d41812666ead
Tomasz Sądej <tomasz@tbx.pl>
2024-06-13 20:29:08 +0200

    smc-v1.3.0

--- Formula/smc.rb
+++ Formula/smc.rb
@@ -1,12 +1,12 @@
 class Smc < Formula
   desc "SMC CLI"
   homepage "https://github.com/tawerna/smc.cli"
-  version "1.2.0"
+  version "1.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tawerna/smc.cli/releases/download/v1.2.0/smc-v1.2.0-darwin-arm64.tar.gz"
-      sha256 "6442d8737aca64aadb0c85d217494bbb01721754130815237c3b88bb1005a825"
+      url "https://github.com/tawerna/smc.cli/releases/download/v1.3.0/smc-v1.3.0-darwin-arm64.tar.gz"
+      sha256 "731c33273f5aa3c7578fbcf51dee57d70495f91a21af3b00bc083f4364e5b8b0"
 
       def install
         bin.install "smc"
